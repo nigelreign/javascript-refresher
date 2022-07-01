99daad7e30a2e0ed577532bf48147cfb114677da
nigelreign <zulunigelb@gmail.com>
2022-07-01 05:56:11 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+// const johnMass = 92,
+//   johnHeight = 1.95;
+// const markMass = 78,
+//   markHeight = 1.69;
+
+// const johnBMI = johnMass / johnHeight ** 2;
+// const markBMI = markMass / markHeight ** 2;
+
+// console.log(johnBMI, markBMI);
+
+// const markHigherBMI = markBMI > johnBMI;
+
+// console.log(markHigherBMI);
+
+// ======================================================
+// String literals
+// ======================================================
+
+const firstName = "Nigel";
+const birthYear =  1994;
+const currentYear = 2022;
+const job = "Software Developer"
+
+const about = `I'm ${firstName} a ${currentYear - birthYear} year old ${job}`;
+
+console.log(about)
+
+
+
